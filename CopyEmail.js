f3b1de0c8ebc5878f9e9be7dd18e7915d7623de2
Jason <jcarter91@gmail.com>
2020-05-11 11:52:35 -0500

    Test

--- CopyEmail.js
+++ CopyEmail.js
@@ -6,11 +6,11 @@ function copyEmail() {
 }
 
 /*
-<div class="copy-email" onclick="copyEmail()">
-        <h2 class="about-link close-contact">CLICK TO COPY</h2>
-        <input style="opacity: 0;" type="text" id="copyEmail" value="jcarter91@gmail.com">
-      </input>
-      </div>
+    <div data-w-id="2c350c4f-2e20-86df-9032-23d8014c9213" class="copy-email" onclick="copyEmail()">
+            <h2 class="about-link close-contact">CLICK TO COPY</h2>
+            <input style="opacity:0;  width:0px;" type="text" id="copyEmail" value="jcarter91@gmail.com">
+          </input>
+          </div>
 
 <script src="/CopyEmail.js" type="text/javascript"></script>
 
