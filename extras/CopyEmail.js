64aaa78f299820d1a2fa29ba133a5a6a2acfc8e0
Jason <jcarter91@gmail.com>
2020-06-21 13:04:03 -0500

    added darkmode

---
+++ extras/CopyEmail.js
@@ -0,0 +1,20 @@
+// function copyEmail() {
+//   var copyText = document.getElementById("copyEmail");
+//   copyText.select();
+//   copyText.setSelectionRange(0, 99999);
+//   document.execCommand("copy");
+//   console.log("copied.");
+// }
+
+/*
+    <div data-w-id="2c350c4f-2e20-86df-9032-23d8014c9213" class="copy-email" onclick="copyEmail()">
+            <h2 class="about-link close-contact">CLICK TO COPY</h2>
+            <input style="opacity:0;  width:0px;" type="text" id="copyEmail" value="jcarter91@gmail.com">
+          </input>
+          </div>
+
+<script src="/CopyEmail.js" type="text/javascript"></script>
+
+ */
+
+//  <video autoplay loop src="/images/CMP.mov" style="width:100%"></video>
