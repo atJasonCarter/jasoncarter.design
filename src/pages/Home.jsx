b8a0c9ed1fed6b0cb9f79565fdbf9f7ccb156827
Jason <jcarter91@gmail.com>
2026-09-23 13:02:43 -0500

    xx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -47,7 +47,14 @@ export default function Home() {
         </>
       )}
 
-      <a  href='/JasonCarterResume26.pdf' target="_blank" download="JasonCarterResume26.pdf" className="resume-link" style={{ marginTop: '96px', marginBottom: '240px' }}>Download Resume</a>
+      <div className="cta-row" style={{ marginTop: '96px', marginBottom: '240px' }}>
+        <a href='/JasonCarterResume26.pdf' target="_blank" download="JasonCarterResume26.pdf" className="resume-link">Download Resume</a>
+        <a href="https://www.linkedin.com/in/atJasonCarter" target="_blank" rel="noopener noreferrer" className="linkedin-link" aria-label="LinkedIn" title="LinkedIn">
+          <svg viewBox="0 0 24 24" fill="currentColor" width="20" height="20">
+            <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+          </svg>
+        </a>
+      </div>
       {/* <div className="breath"></div> */}
           {/* <!-- family images --> */}
      <div className="Familyimages">
